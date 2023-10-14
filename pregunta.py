51566858e2314e80505a3a192ba221ab95a0d18f
johanUNAL753 <jmarincr@unal.edu.co>
2023-10-14 17:41:28 -0500

    Laboratorio de ingestion
    Envio todos los puntos

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,36 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
+    with open('clusters_report.txt') as f:
+        lines = [line.strip() for line in f.readlines()[4:]]
 
-    #
-    # Inserte su código aquí
-    #
+    concatenated_lines = []
+    current_line = ''
+    for line in lines:
+        if line:
+            current_line += ' ' + line
+        else:
+            concatenated_lines.append(current_line.strip())
+            current_line = ''
+
+    processed_data = []
+    for line in concatenated_lines:
+        match = re.search(r'(^[0-9]+)\W+([0-9]+)\W+([0-9]+)([!#$%&*+-.^_`|~:\[\]]+)(\d+)(\W+)(.+)', line)
+        result_line = match.group(1) + '*' + match.group(2) + '*' + match.group(3) + '.' + match.group(5) + '*' + match.group(7)
+        processed_data.append(result_line)
+
+    data_set = [line.split('*') for line in processed_data]
+    df = pd.DataFrame(data_set, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+
+    df['cluster'] = df['cluster'].astype(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(float)
+
+    principales_palabras_clave = df['principales_palabras_clave'].str.replace('    ', ' ').str.replace('   ', ' ').str.replace('  ', ' ').str.replace('.', '').str.split(',')
+    df['principales_palabras_clave'] = principales_palabras_clave.apply(lambda x: ', '.join(map(str.strip, x)))
 
     return df
